76e910f60d3a63f2c6fabadacff5a284c37aed72
pez3002 <ametatheton2@gmail.com>
2024-05-27 17:27:11 -0700

    Creating database

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
-    "github.com/joelewaldo/jinks-backend/cmd/api"
-    "log"
+	"database/sql"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joelewaldo/jinks-backend/cmd/api"
+	"github.com/joelewaldo/jinks-backend/conifg"
+	"github.com/joelewaldo/jinks-backend/db"
 )
 
 func main() {
+    db, err := db.NewSQLDb(mysql.Config{
+        User: config.Envs.DBUser,
+        Passwd: config.Envs.DBPassword,
+        Addr: config.Envs.DBAddress,
+        DBName: config.Envs.DBName,
+        Net: "tcp",
+        AllowNativePasswords: true,
+        ParseTime: true,
+    })
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    initStorage(db)
+
     server := api.NewApiServer(":8080", nil)
     if err := server.Run(); err != nil {
         log.Fatal(err)
     }
+}
+
+func initStorage(db *sql.DB) {
+    err := db.Ping()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Println("DB: Successfully Connected!")
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,10 @@ module github.com/joelewaldo/jinks-backend
 
 go 1.22.3
 
+require github.com/gorilla/mux v1.8.1
+
 require (
-    github.com/gorilla/mux v1.8.1
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
